4e411a16505229d2e89d2154efe7b411cc7c1750
David Martos Ruiz <david@davidmartos.dev>
2023-04-10 12:43:32 +0200

    Nuevo fichero

---
+++ Traducciones.java
@@ -0,0 +1,23 @@
+/**
+ * Traducciones
+ * Ejercicio 3
+ *
+ * Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente traducción al castellano. Las palabras deben estar distribuidas en dos columnas y alineadas a la izquierda.
+ *
+ * @author Martos Ruiz, David
+ * Fecha: 22/09/2022
+ */
+public class Traducciones{
+  public static void main(String[] args) {
+    System.out.printf("%-10s ordenador\n", "computer");
+    System.out.printf("%-10s alumno\\a\n", "student");
+    System.out.printf("%-10s gato\n", "cat");
+    System.out.printf("%-10s pingüino\n", "penguin");
+    System.out.printf("%-10s máquina\n", "machine");
+    System.out.printf("%-10s naturaleza\n", "nature");
+    System.out.printf("%-10s luz\n", "light");
+    System.out.printf("%-10s verde\n", "green");
+    System.out.printf("%-10s libro\n", "book");
+    System.out.printf("%-10s pirámide\n", "pyramid");
+  }
+}
