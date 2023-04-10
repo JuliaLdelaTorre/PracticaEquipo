69293968ed6d46913f22462e4f0e1891168c66aa
David Martos Ruiz <david@davidmartos.dev>
2023-04-10 12:39:00 +0200

    Cambios david

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ *
+ * @author Martos Ruiz, David
+ * Fecha: 2022/09/21
+ */
+public class HolaMundo { // Clase principal
+    public static void main(String[] args) {
+    System.out.println("¡Hola mundo!");
+  }
+}
